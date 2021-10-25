0d098bb6cc3a20358f6a96c92a321f71a9802194
Aryan-Malik-2000 <a3malik@ucsd.edu>
2021-10-24 17:01:04 -0700

    explore.js complete

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,51 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var synth = window.speechSynthesis;
+  var voiceSelect = document.querySelector('select');
+  var PressToTalk = document.querySelector('button');
+  var voices = [];
+
+  // Got from developer.mozille.org
+  function populateVoices() {
+    voices = synth.getVoices();
+  
+    for(var i = 0; i < voices.length; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name;
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  PressToTalk.addEventListener('click', (event) => {
+    var smileimage = document.querySelector('img');
+    var text = document.querySelector('textarea').value;
+    var utterThis = new SpeechSynthesisUtterance(text);
+
+    utterThis.onstart = function (event) {
+      smileimage.src = "assets/images/smiling-open.png";
+    }
+
+    utterThis.onend = function (event) {
+      smileimage.src = "assets/images/smiling.png";
+    }
+
+
+    var voiceoption = document.querySelector('select').value;
+    for (var i = 0; i < voices.length; i++) {
+      if (voices[i].name == voiceoption) {
+        utterThis.voice = voice[i];
+      }
+    }
+    synth.speak(utterThis);
+
+  }
+
 }
